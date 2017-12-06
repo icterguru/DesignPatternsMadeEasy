a9ab042aa4e9ee7f5feed469fb4d7509baca1498
Dr.Mokter <mokter@gmail.com>
2017-12-05 21:28:01 -0500

    Source code added

---
+++ src/designPatterns_Examples/AllPatternNames.java
@@ -0,0 +1,11 @@
+public class AllPatternNames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creational Patterns: FASBP");
+		System.out.println("Structural1 Patterns: ABCDFFP");
+		System.out.println("Behavioral Patterns: CCIIMMNOSSTV");
+
+	}
+
+}
